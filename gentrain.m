ae16ef3749b4b41d5ee369d4b855c4cc64f9cb0c
Benjamin X. Liu <benliu961@gmail.com>
2023-06-05 17:38:19 -0400

    generated some images

--- gentrain.m
+++ gentrain.m
@@ -10,22 +10,23 @@ function gentrain(indices)
 % Written by X on Y
 
 % Run something with all defaults
-[Hx,th0,params]=simulosl;
 
+[Hx,th0,params]=simulosl;
 xver=1;
 
 % Change something
 th0(1)=1.5e6;
+th0(2)=1.5;
+th0(3)=10000;
 
 dth=1e5;
 for index=indices
  th0(1)=th0(1)+dth;
- params.NyNx=[128 128]; 
  [Hx,th0,params]=simulosl(th0,params);
 
  % Generic name
  fname=sprintf('gentrain_%.3i',index);
-
+ oldFolder = cd('trainingdataimages');
  if xver==1
   % Make a quick plot
   imagesc(v2s(Hx,params))
@@ -33,12 +34,15 @@ for index=indices
 		 th0(1),th0(2),th0(3),...
 		 params.dydx(1),params.dydx(2),...
                  params.NyNx(1),params.NyNx(2)))
-  pause(1)
-  print('-dpdf',fname)
+  set(gca, 'visible', 'off')
+  set(gca, 'xtick', [])
+  print('-dpng',fname)
  end
-
+ cd(oldFolder)
+ cd('trainingdatavalues')
  % Write these fake data to a file
  save(fname,'Hx','th0','params')
+ cd(oldFolder)
 end
 
 
