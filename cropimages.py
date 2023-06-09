e8784dbd1c88bf736f367c73cf096b6d1f13910f
benliu961 <benliu961@gmail.com>
2023-06-09 10:55:52 -0400

    cropped white border

--- cropimages.py
+++ cropimages.py
@@ -1,9 +1,27 @@
 # import required module
 import os
 from PIL import Image
+import numpy as np
  
 
 directory = 'trainingdataimages'
+
+def find_white_corners(im):
+    data = np.asarray(im)
+    x = np.argwhere(data != 255)
+    # print(x)
+    left = x[0][1]
+    top = x[0][0]
+    right = x[-1][1]
+    bottom = x[-1][0]
+    return left, top, right, bottom
+
+# im = Image.open('trainingdataimages/gentrain_001.png')
+# left, top, right, bottom = find_white_corners(im)
+
+# im1 = im.crop((left, top, right, bottom))
+
+# im1.save("test.png","PNG")
  
 # iterate over files in
 # that directory
@@ -16,13 +34,10 @@ for filename in os.listdir(directory):
         
         # Size of the image in pixels (size of original image)
         # (This is not mandatory)
-        width, height = im.size
+        # width, height = im.size
         
         # Setting the points for cropped image
-        left = 116
-        top = 48
-        right = 806
-        bottom = 574
+        left, top, right, bottom = find_white_corners(im)
 
         # Cropped image of above dimension
         # (It will not change original image)
